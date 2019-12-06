ecfe6334d7dba36c5574fc738cfbaf32609976b2
Camatoz <yurabondik99@gmail.com>
2019-12-06 16:12:37 +0300

    start poject

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(window).load(function () {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        start: function () {
+            sliderContentPosition();
+        }
+    });
+});
+
+//////////////////////////////////
+function sliderContentPosition() {
+
+    let containerLeftPos = $('header div.container').position().left,
+        headButton = $('div.head-button'),
+        dots = $('ol.flex-control-nav');
+
+    headButton.css('paddingLeft', containerLeftPos);
+    dots.css('paddingLeft', containerLeftPos);
+
+}
+
+$(window).resize(function () {
+    sliderContentPosition();
+});
