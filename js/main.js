2a5c5e7cc395754d83f39472aaa60f2831368296
Camatoz <yurabondik99@gmail.com>
2020-01-07 12:05:40 +0200

    Tour slider

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(window).load(function () {
   initScroll();
   initSandwich();
   initSubMenu();
+  initTourSlider();
+  initTourSelector();
 
 });
 
@@ -25,7 +27,7 @@ $(window).resize(function () {
 ///////////////////////////
 function initMainSlider() {
 
-  $(".flexslider").flexslider({
+  $("header div.flexslider").flexslider({
     animation: "slide",
     start: function () {
       sliderContentPosition();
@@ -44,11 +46,12 @@ function initScroll() {
 /////////////////////////
 function initSandwich() {
   let sandwich = $(".head-nav-sandwich"),
-    menu = $(".head-nav"),
-    menuTopPos = sandwich.position().top + sandwich.height();
+    menu = $(".head-nav");
+
 
 
   sandwich.click(function () {
+    let menuTopPos = sandwich.position().top + sandwich.height();
     menu.css("top", menuTopPos)
     menu.slideToggle(100);
     return false;
@@ -64,7 +67,61 @@ function initSubMenu() {
 
     let item = $(this),
       sub = item.next('.head-nav-sub');
-      sub.slideToggle(100);
+    sub.slideToggle(100);
   })
+
+}
+
+///////////////////////////
+function initTourSlider() {
+
+  $(" div.hot-tour-slider").flexslider({
+    animation: "slide",
+    move: 1,
+    minItems: 3,
+    maxItems: 3,
+    itemWidth: 300,
+    itemMargin: 20,
+    controlNav: false,
+    delay: 3000
+  });
+
+}
+
+///////////////////////////
+function initPopSlider() {
+
+  $(" div.pop-tour-slider").flexslider({
+    animation: "slide",
+    move: 1,
+    minItems: 3,
+    maxItems: 3,
+    itemWidth: 300,
+    itemMargin: 20,
+    controlNav: false,
+    delay: 3000
+  });
+
+}
+
+/////////////////////////////
+function initTourSelector() {
+
+  let hotBtn = $('a.hot-tour-btn'),
+    popBtn = $('a.pop-tour-btn'),
+    hotSlider = $('div.hot-tour-slider'),
+    popSlider = $('div.pop-tour-slider');
+
+  hotBtn.click(function () {
+    hotSlider.show(100);
+    popSlider.hide(100);
+    return false
+  })
+
+  popBtn.click(function () {
+    popSlider.show(100, initPopSlider);
+    hotSlider.hide(100);
+    return false    
+  });
 
 }
